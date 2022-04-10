83be0f8c46b874c41130b5a25ce3d32b6d0a0784
Richard Zozimo <richardzozimo@gmail.com>
2022-04-09 23:23:11 -0300

    Criando componentes e construindo projeto Angular

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,37 @@
+﻿import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 99.90,
+        code: 'XPS-8932',
+        durations: 120,
+        rating: 5.4,
+        releaseDate: 'December, 2, 2019'
+      },
+      {
+        id: 2,
+        name: 'Angular: Http',
+        imageUrl: '',
+        price: 47.90,
+        code: 'LKL-4921',
+        durations: 80,
+        rating: 4.1,
+        releaseDate: 'January, 21, 2020'
+      }
+  ]
+  }
+
+}
---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+﻿export class Course {
+  id: number | undefined;
+  name: string | undefined;
+  imageUrl: string | undefined;
+  price: number | undefined;
+  code: string | undefined;
+  durations: number | undefined;
+  rating: number | undefined;
+  releaseDate: string | undefined;
+}
