f833567d1c1b92c52755e2b6128f8de1c21683e6
Richard Zozimo <richardzozimo@gmail.com>
2022-04-12 19:01:25 -0300

    corrigindo estrutura de cursos

--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -1,10 +1,21 @@
 ﻿export class Course {
-  id: number | undefined;
-  name: string | undefined;
-  imageUrl: string | undefined;
-  price: number | undefined;
-  code: string | undefined;
-  duration: number | undefined;
-  rating!: number;
-  releaseDate: string | undefined;
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  code: string;
+  duration: number;
+  rating: number;
+  releaseDate: string;
+
+  constructor(id: number, name: string, imageUrl: string, price: number, code: string, duration: number, rating: number, releaseDate: string) {
+    this.id = id;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.code = code;
+    this.duration = duration;
+    this.rating = rating;
+    this.releaseDate = releaseDate;
+  }
 }
