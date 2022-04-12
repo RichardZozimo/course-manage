439beb56d84846a6c3152954da161e3b0b7e35ff
Richard Zozimo <richardzozimo@gmail.com>
2022-04-12 19:11:48 -0300

    injeção de dependencia

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
   selector: 'app-course-list',
@@ -9,29 +10,10 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) {}
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 99.90,
-        code: 'XPS-8932',
-        duration: 120,
-        rating: 4.4,
-        releaseDate: 'December, 2, 2019'
-      },
-      {
-        id: 2,
-        name: 'Angular: Http',
-        imageUrl: '/assets/images/http.png',
-        price: 47.90,
-        code: 'LKL-4921',
-        duration: 80,
-        rating: 3.1,
-        releaseDate: 'January, 21, 2020'
-      }
-  ]
+    this.courses = this.courseService.retrieveAll();
   }
 
 }
