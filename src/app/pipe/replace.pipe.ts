2ce295825f552a50845444792f8fbd9bc7b286ab
Richard Zozimo <richardzozimo@gmail.com>
2022-04-12 21:33:30 -0300

    utilizando pipes para configuração de exibicao

---
+++ src/app/pipe/replace.pipe.ts
@@ -0,0 +1,12 @@
+﻿import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: 'replace' // nome utilizado na chamada do pipe
+})
+export class ReplacePipe implements PipeTransform {
+
+  transform(value: string, char: string, valueToReplace: string) {
+    return value.replace(char, valueToReplace);
+  }
+
+}
